5890ab22d46cf4ca12a50eb6ce057654e018607c
Fernando Horas <fernando.horas@bspcloud.com>
2022-08-13 19:39:18 -0300

    Upgrade no dir concorrência

---
+++ concorrencia/select/select.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"time"
+)
+
+func oMaisRapido(url1, url2, url3 string) string {
+	c1 := html.Titulo(url1)
+	c2 := html.Titulo(url2)
+	c3 := html.Titulo(url3)
+
+
+	// estrutura de controle especifica para concorrência
+
+	select {
+	case t1 := <-c1:
+		return t1
+	case t2 := <-c2:
+		return t2
+	case t3 := <-c3:
+		return t3
+	case <-time.After(1000 * time.Microsecond):
+		return "Tods perderam"
+	default:
+		return "Sem resposta ainda!"
+
+	}
+
+
+}
+
+func main()  {
+	campeao := oMaisRapido(
+		"https://www.youtube.com",
+		"https://www.amazon.com",
+		"https://www.google.com",
+	)
+	fmt.Println(campeao)
+	
+	}	
